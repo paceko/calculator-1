f7c8d2d0c017e71dbe2ce72c962f8b510833a9f2
Hackbright Student <info@hackbrightacademy.com>
2016-04-06 12:54:00 -0700

    refined code

--- arithmetic.py
+++ arithmetic.py
@@ -1,32 +1,26 @@
 def add(num1, num2):
     """ Returns the sum of the two input integers"""
-    total = num1 + num2 
-    return total 
+    return num1 + num2 
 
 def subtract(num1, num2):
     """Returns the second number subtracted from the first"""
-    total = num1 - num2
-    return total
+    return num1 - num2
 
 def multiply(num1, num2):
     """Multiplies the two inputs together"""
-    total = num1 * num2
-    return total
+    return num1 * num2
 
 def divide(num1, num2):
     """Divides the first input by the second, returning a floating point"""
-    total = float(num1) / num2
-    return total
+    return float(num1) / num2
 
 def square(num1):
     """Returns the square of the input"""
-    total = num1 ** 2
-    return total
+    return num1 ** 2
 
 def cube(num1):
     """Returns the cube of the input"""
-    total = num1 ** 3
-    return total
+    return num1 ** 3
 
 def power(num1, num2):
     """Raises the first integer to the power of the second integer and returns the value."""
