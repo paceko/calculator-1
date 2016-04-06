6dbc96d7d0c0123aa5492af8167fc488092d95db
Hackbright Student <info@hackbrightacademy.com>
2016-04-06 12:35:36 -0700

    add thru cube completed

---
+++ arithmetic.py
@@ -0,0 +1,34 @@
+def add(num1, num2):
+    """ Returns the sum of the two input integers"""
+    total = num1 + num2 
+    return total 
+
+def subtract(num1, num2):
+    """Returns the second number subtracted from the first"""
+    total = num1 - num2
+    return total
+
+def multiply(num1, num2):
+    """Multiplies the two inputs together"""
+    total = num1 * num2
+    return total
+
+def divide(num1, num2):
+    """Divides the first input by the second, returning a floating point"""
+    total = float(num1) / num2
+    return total
+
+def square(num1):
+    """Returns the square of the input"""
+    total = num1 ** 2
+    return total
+
+def cube(num1):
+    """Returns the cube of the input"""
+    total = num1 ** 3
+
+def power(num1, num2):
+    pass
+
+def mod(num1, num2):
+    pass
